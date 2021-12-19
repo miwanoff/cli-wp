42770d12bb74d4f6fe5a05fb9d3cfebfe316003d
miwanoff <marina.iva.noffa@gmail.com>
2021-12-19 13:51:12 +0200

    add bootstrap scripts and jquery

--- wp-content/themes/bootkit/index.php
+++ wp-content/themes/bootkit/index.php
@@ -296,9 +296,7 @@
         <!-- /.container -->
     </footer>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <?php wp_footer();?>
 
 </body>
 
