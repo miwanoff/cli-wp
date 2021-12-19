a46fb7652b69ab9c088ca6d9a9d1e83f8a03a8e1
miwanoff <marina.iva.noffa@gmail.com>
2021-12-19 13:19:39 +0200

    add and include enqueue.php

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -1,25 +1,9 @@
 <?php
-function my_filter_function1($str)
-{
-    $str = '<em>' . $str . '</em>';
-    return $str; // возвращаем измененное значение!
-}
+// Setup
 
-add_filter('my_filter1', 'my_filter_function1');
+// Includes
+include get_theme_file_path('includes/enqueue.php');
 
-function my_filter_function2($str)
-{
-    return 'Hello ' . $str . '!'; // возвращаем измененное значение!
-}
-add_filter('my_filter2', 'my_filter_function2');
+// Hooks
 
-function my_action_function($text)
-{
-    echo 'Произошло событие "my_action"!'; // ничего не возвращаем!
-}
-add_action('my_action', 'my_action_function', 2);
-function my_action_function_new()
-{
-    echo "!!!";
-}
-add_action('my_action', 'my_action_function_new', 1);
+// Shortcodes
