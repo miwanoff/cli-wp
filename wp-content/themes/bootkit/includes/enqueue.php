bf43d5dea593fb7ef6d071eac5dd2b6950268109
miwanoff <marina.iva.noffa@gmail.com>
2022-01-16 14:02:08 +0200

    add my_bootkit css

--- wp-content/themes/bootkit/includes/enqueue.php
+++ wp-content/themes/bootkit/includes/enqueue.php
@@ -7,12 +7,13 @@ function bootkit_enqueue()
     wp_register_style('bootkit_google_fonts', 'https://fonts.googleapis.com/css?family=Open+Sans&display=swap', [], $ver); // Google fonts
     wp_register_style('bootkit_bootstrap', $url . '/assets/vendor/bootstrap/css/bootstrap.min.css', [], $ver); // bootstrap.min.css
     wp_register_style('bootkit_modern_business', $url . '/assets/css/modern-business.css');
+    wp_register_style('my_bootkit', $url . '/css/my_bootkit.css');
     // modern-business.css
     wp_enqueue_style('bootkit_google_fonts');
     wp_enqueue_style('bootkit_bootstrap');
     wp_enqueue_style('bootkit_modern_business');
     wp_enqueue_style('style', get_stylesheet_uri());
-
+    wp_enqueue_style('my_bootkit');
     //----Scripts-----------
     wp_register_script('bootkit_bootstrap', $url .
         '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', 'jquery', $ver, true);
