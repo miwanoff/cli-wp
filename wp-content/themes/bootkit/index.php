b48cb2a11e3375a70a25439727dcac6ce8dfd19b
miwanoff <marina.iva.noffa@gmail.com>
2022-01-16 14:33:25 +0200

    add pagination

--- wp-content/themes/bootkit/index.php
+++ wp-content/themes/bootkit/index.php
@@ -25,10 +25,12 @@ get_header();
             <!-- Pagination -->
             <ul class="pagination justify-content-center mb-4">
                 <li class="page-item">
-                    <a class="page-link" href="#">&larr; Older</a>
+                    <?php previous_posts_link("&larr; Older");?>
+                    <!-- <a class="page-link" href="#">&larr; Older</a> -->
                 </li>
                 <li class="page-item disabled">
-                    <a class="page-link" href="#">Newer &rarr;</a>
+                    <?php next_posts_link("Newer &rarr;");?>
+                    <!-- <a class="page-link" href="#">Newer &rarr;</a> -->
                 </li>
             </ul>
 
