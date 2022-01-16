f4592b2468103c85a471c3022d11bd132ea9d8ed
miwanoff <marina.iva.noffa@gmail.com>
2022-01-16 14:44:48 +0200

    add single template and change header.php

---
+++ wp-content/themes/bootkit/single.php
@@ -0,0 +1,38 @@
+<?php
+get_header();
+?>
+<!-- Page Content -->
+<div class="container">
+
+
+    <!-- Marketing Icons Section -->
+    <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+            <?php if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+
+        get_template_part('partials/posts/content', 'excerpt');
+
+    }
+}
+?>
+
+
+
+
+        </div>
+
+        <?php get_sidebar();?>
+    </div>
+    <!-- /.row -->
+    <hr>
+
+
+</div>
+<!-- /.container -->
+<?php
+get_footer();
+?>
