baac20c71fd9418c471185257cdf141e2cd3f984
miwanoff <marina.iva.noffa@gmail.com>
2022-01-23 14:19:25 +0200

    comments was added

--- wp-content/themes/bootkit/single.php
+++ wp-content/themes/bootkit/single.php
@@ -13,10 +13,71 @@ get_header();
             <?php if (have_posts()) {
     while (have_posts()) {
         the_post();
+        global $post;
+        $author_ID = $post->post_author;
+        $author_URL = get_author_posts_url($author_ID);
+        ?>
+            <!-- Title -->
+            <h1 class="mt-4"><?php the_title()?></h1>
+            <!-- Post category: -->
+            <h2 class="mt-4"><?php the_category(" ")?></h2>
 
-        get_template_part('partials/posts/content', 'excerpt');
+            <!-- Date/Time -->
+            <p><?php the_time(get_option('date_format'));
+        echo " ";
+        the_time(get_option('time_format'));?></p>
+            <hr>
 
-    }
+
+            <!-- Author -->
+            <p class="lead">
+                by
+                <a href="<?php echo $author_URL; ?>"><?php the_author();?></a>
+            </p>
+            <hr>
+
+            <!-- Preview Image -->
+            <?php
+if (has_post_thumbnail()) {
+            the_post_thumbnail("full", ["class" => "card-img-top"]);
+        }
+        ?>
+            <hr>
+
+
+            <!-- Post Content -->
+            <?php the_content();
+
+        $defaults = array(
+            'before' => '<div class="row justify-content-center align-items-center">' . __('Pages:'),
+            'after' => '</div>',
+        );
+        wp_link_pages($defaults);
+        edit_post_link();
+        ?>
+
+
+            <hr>
+            <!-- Tag cloud -->
+            <?php the_tags('', ', ');?>
+            <hr>
+            <!-- Pagination -->
+            <ul class="pagination justify-content-center mb-4">
+                <li class="page-item">
+                    <?php previous_post_link();?>
+                </li>
+                <li class="page-item">
+                    <?php next_post_link();?>
+                </li>
+            </ul>
+
+            <?php
+if (comments_open() || get_comments_number()) {
+            comments_template();
+        }
+        ?>
+            <?php
+}
 }
 ?>
 
