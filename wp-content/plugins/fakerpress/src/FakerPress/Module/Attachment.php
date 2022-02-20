85c9a9f5c3e89a43efd95d5a77c3fc0403683d97
miwanoff <marina.iva.noffa@gmail.com>
2022-02-20 12:49:04 +0200

    Plugins updated

--- wp-content/plugins/fakerpress/src/FakerPress/Module/Attachment.php
+++ wp-content/plugins/fakerpress/src/FakerPress/Module/Attachment.php
@@ -1,5 +1,6 @@
 <?php
 namespace FakerPress\Module;
+use WP_Error;
 use FakerPress;
 use Faker;
 
@@ -59,13 +60,8 @@ class Attachment extends Base {
 		// Download temp file
 		$temporary_file = download_url( $url, 10 );
 
-
-
 		// Check for download errors if there are error unlink the temp file name
 		if ( is_wp_error( $temporary_file ) ) {
-			if ( PHP_MAJOR_VERSION <= 8 ) {
-				@unlink( $temporary_file );
-			}
 			return $temporary_file;
 		}
 
