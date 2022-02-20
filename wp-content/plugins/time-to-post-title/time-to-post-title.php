85ae6ca9b23a21650bd27e9d109a270181cbbfa0
miwanoff <marina.iva.noffa@gmail.com>
2022-02-20 15:11:37 +0200

    add po and mo files

--- wp-content/plugins/time-to-post-title/time-to-post-title.php
+++ wp-content/plugins/time-to-post-title/time-to-post-title.php
@@ -11,6 +11,12 @@ License: GPL2
 
 function time_to_post_content($content)
 {
-    return '<div class="time">' . __('Time of post creation: ') . get_the_time() . " " . get_the_date() . '</div>' . $content;
+    return '<div class="time">' . __('Time of post creation: ', 'time-to-post-title') . get_the_time() . " " . __("Date of post creation:", 'time-to-post-title') . get_the_date() . '</div>' . $content;
 }
 add_filter('the_content', 'time_to_post_content');
+
+add_action('plugins_loaded', 'time_load_plugin_textdomain');
+function time_load_plugin_textdomain()
+{
+    load_plugin_textdomain('time-to-post-title', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+}
