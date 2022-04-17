2545432072d51c87990315316a9ec78c93a7671f
miwanoff <marina.iva.noffa@gmail.com>
2022-04-17 16:53:57 +0300

    add widget area

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -46,3 +46,14 @@ function bootkit_ajax()
     echo $summa;
     die;
 }
+
+//-----------------
+if (function_exists('register_sidebars')) {
+    register_sidebar(array(
+        'name' => 'Sidebar Area',
+        'before_widget' => '<div id="%1$s" class="backgroundlist %2$s"><div class="listtitle">',
+        'after_widget' => '</div></div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2></div><div class="contentbox">',
+    ));
+}
--- wp-content/themes/bootkit/sidebar.php
+++ wp-content/themes/bootkit/sidebar.php
@@ -4,4 +4,11 @@
      <?php if (is_active_sidebar('bootkit_sidebar')) {
     dynamic_sidebar('bootkit_sidebar');
 }?>
+     <div class="my-new-sidebar">
+
+         <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar("Sidebar Area")): ?>
+
+         <?php endif;?>
+
+     </div>
  </div>
