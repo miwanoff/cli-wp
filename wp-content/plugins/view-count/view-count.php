ceb7fa2df0b7963f4cc9acda0a104ccec35586b3
miwanoff <marina.iva.noffa@gmail.com>
2022-04-23 16:31:50 +0300

    add uninstall_view_count

--- wp-content/plugins/view-count/view-count.php
+++ wp-content/plugins/view-count/view-count.php
@@ -54,3 +54,20 @@ function update_view_count()
     );
 }
 add_action('wp_head', 'update_view_count');
+
+// function uninstall_view_count()
+// {
+//     global $wpdb;
+//     $query = "SELECT view_count FROM wp_posts";
+//     $result = $wpdb->get_row($query);
+//     if (is_null($result)) {
+//         return;
+//     }
+//     $query = "ALTER TABLE
+//               " . $wpdb->posts . "
+//           DROP
+//               `view_count`";
+//     $wpdb->query($query);
+// }
+
+// register_uninstall_hook(__FILE__, 'uninstall_view_count');
