305f955d23d9566f45512bd1f65a3378b2d85668
miwanoff <marina.iva.noffa@gmail.com>
2022-04-23 15:50:29 +0300

    changed message to src

--- wp-content/plugins/advertisement-widget-plugin.php
+++ wp-content/plugins/advertisement-widget-plugin.php
@@ -26,7 +26,7 @@ class Advertisement_Widget extends WP_Widget
     {
         $instance = $old_instance;
         $instance['title'] = $new_instance['title'];
-        $instance['message'] = $new_instance['message'];
+        $instance['src'] = $new_instance['src'];
         $instance['link'] = $new_instance['link'];
         return $instance;
     }
@@ -36,7 +36,7 @@ class Advertisement_Widget extends WP_Widget
     {
         extract($args);
         $title = apply_filters('widget_title', $instance['title']);
-        $message = $instance['message'];
+        $src = $instance['src'];
         $link = $instance['link'];
         ?>
 <?php echo $before_widget; ?>
@@ -44,7 +44,7 @@ class Advertisement_Widget extends WP_Widget
             echo $before_title . $title . $after_title;
         }
         ?>
-<?php echo '<a href="' . $link . '" target="_blank"><img src=' . $message . " ' style=\"margin-top:20px;\"/>"; ?></a>
+<?php echo '<a href="' . $link . '" target="_blank"><img src=' . $src . " ' style=\"margin-top:20px;\"/>"; ?></a>
 <?php echo $after_widget; ?>
 <?php
 }
@@ -54,15 +54,15 @@ class Advertisement_Widget extends WP_Widget
     {
         $instance = wp_parse_args((array) $instance, array('title' => ''));
         $title = $instance['title'];
-        $message = esc_attr($instance['message']);
+        $src = esc_attr($instance['src']);
         $link = esc_attr($instance['link']);
         ?>
 <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat"
             id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>"
             type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
-<p><label for="<?php echo $this->get_field_id('message'); ?>"><?php _e('Advertisement Banner');?></label>
-    <textarea rows="4" cols="50" class="widefat" id="<?php echo $this->get_field_id('message'); ?>"
-        name="<?php echo $this->get_field_name('message'); ?>"><?php echo ($message); ?> </textarea>
+<p><label for="<?php echo $this->get_field_id('src'); ?>"><?php _e('Advertisement Banner');?></label>
+    <textarea rows="4" cols="50" class="widefat" id="<?php echo $this->get_field_id('src'); ?>"
+        name="<?php echo $this->get_field_name('src'); ?>"><?php echo ($src); ?> </textarea>
 </p>
 <label for="<?php echo $this->get_field_id('link'); ?>"><?php _e('Ads Link');?></label>
 <input class="widefat" id="<?php echo $this->get_field_id('link'); ?>"
