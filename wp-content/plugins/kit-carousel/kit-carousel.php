54d692f5d73457ac98db5b59ffa29e2e85eb0bfb
miwanoff <marina.iva.noffa@gmail.com>
2022-05-01 15:44:00 +0300

    Kit Carousel plugin added

---
+++ wp-content/plugins/kit-carousel/kit-carousel.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Plugin Name: Kit Carousel
+ * Plugin URI:
+ * Description: Simple carousel
+ * Version: 1.0
+ * Author: MAI
+ * Author URI: https://github.com/miwanoff/
+ * License:           GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+ 
