c5ae9e5fd26ebf8984ddd7a8a86f69dfc95d4a9d
miwanoff <marina.iva.noffa@gmail.com>
2022-05-08 17:24:38 +0300

    add hooks

--- wp-content/plugins/kit-copyright/includes/class-kit-copyright.php
+++ wp-content/plugins/kit-copyright/includes/class-kit-copyright.php
@@ -186,7 +186,7 @@ class Kit_Copyright
 
         $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
         $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
-
+        $this->loader->add_action('wp_footer', $plugin_public, 'add_text_footer');
     }
 
     /**
