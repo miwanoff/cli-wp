8c78c8cdd8c33b5c515b1d33c7ae207af0a696f7
miwanoff <marina.iva.noffa@gmail.com>
2022-05-08 15:24:14 +0300

    add day post plugin folder and random-post.php

---
+++ wp-content/plugins/day-post/random-post.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Plugin Name: Post of the day
+ * Plugin URI:
+ * Description: Random post of the day
+ * Version: 1.0
+ * Author: MAI
+ * Author URI: https://github.com/miwanoff/
+ * License:           GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// Make sure we don't expose any info if called directly
+if (!function_exists('add_action')) {
+    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+    exit;
+}
+
+// Setup
+
+// Includes
+
+// Hooks
+
+// Shortcodes
